3931e181ab4467492f5eaaf5b84a926b9c29936b
Some1isHeree <86526585+Some1isHeree@users.noreply.github.com>
2022-03-23 16:52:51 +0100

    Update bot.lua

--- support bot/bot.lua
+++ support bot/bot.lua
@@ -7,7 +7,7 @@ end)
 
 client:on('messageCreate', function(message)
 	if message.content == 's!info' then
-		message.channel:send('Hello! I am Your Name Support bot! \nContact:\nMail: Your Mail\nDiscord: Your dc nick and ID\nDiscord Server: your server')
+		message.channel:send('Hello! I am PUTYOURNAME Support bot! \nContact:\nMail: Your Mail\nDiscord: PUTYOURDCTAGANDNAME\nDiscord Server: your server')
 	end
 end)
 

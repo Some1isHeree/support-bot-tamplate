ccb73f09081603e0057701473b2b39ed4228a9c5
triple x <80281764+oktriplex@users.noreply.github.com>
2022-04-25 03:01:40 +0200

    Update bot.lua

--- support bot/bot.lua
+++ support bot/bot.lua
@@ -9,45 +9,12 @@ client:on('messageCreate', function(message)
 	if message.content == 's!info' then
 		message.channel:send('Hello! I am PUTYOURNAME Support bot! \nContact:\nMail: Your Mail\nDiscord: PUTYOURDCTAGANDNAME\nDiscord Server: your server')
 	end
-end)
-
-client:on('messageCreate', function(message)
 	if message.content == 's!help' then
 		message.channel:send('s!help - Displays Commands\ns!info - Displays info about user\ns!link - Create your own support bot!')
 	end
-end)
-
-client:on('messageCreate', function(message)
 	if message.content == 's!link' then
 		message.channel:send('https://github.com/Some1isHeree/support-bot-tamplate\nCreate your own Support Bot today!')
 	end
 end)
 
 client:run('Bot TOKEN')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
